b6a2a5565878006f6520ca13a5fa795c5a05063f
pacodinucci <franciscoldinucci@gmail.com>
2024-10-24 19:26:08 -0300

    cell selection update

--- src/screens/TableDetail.tsx
+++ src/screens/TableDetail.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   View,
   TextInput,
-  Button,
   ScrollView,
   Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
   StyleSheet,
 } from "react-native";
+import { PlusCircle } from "lucide-react-native";
 
 // Función inicial para generar letras de las columnas
 const generateInitialColumnHeaders = (numColumns) => {
@@ -32,18 +32,16 @@ export default function TableDetail({ route }) {
   const initialColumnHeaders = generateInitialColumnHeaders(2); // 2 columnas (A, B)
   const initialRowHeaders = [1, 2, 3]; // 3 filas (1, 2, 3)
 
-  // Estado para almacenar tanto las filas, como las celdas identificadoras
   const [rows, setRows] = useState(initialRows); // Inicializamos con 3 filas y 2 columnas
   const [columnHeaders, setColumnHeaders] = useState(initialColumnHeaders); // Inicializamos con 2 columnas
   const [rowHeaders, setRowHeaders] = useState(initialRowHeaders); // Inicializamos con 3 filas
 
-  // Estado para controlar qué celda identificadora está en modo edición
-  const [editingColumnIndex, setEditingColumnIndex] = useState(null);
-  const [editingRowIndex, setEditingRowIndex] = useState(null);
+  // Referencia para los inputs
+  const inputRefs = useRef([]); // Array de referencias para todos los inputs
 
-  // Estados para controlar la selección de filas, columnas y celdas
-  const [selectedColumn, setSelectedColumn] = useState(null);
+  // Estados para la selección
   const [selectedRow, setSelectedRow] = useState(null);
+  const [selectedColumn, setSelectedColumn] = useState(null);
   const [selectedCell, setSelectedCell] = useState({ row: null, col: null });
 
   // Función para actualizar una celda en la tabla
@@ -72,207 +70,195 @@ export default function TableDetail({ route }) {
     ]); // Agregar la siguiente letra del abecedario
   };
 
-  // Función para actualizar el valor de un encabezado de columna
-  const updateColumnHeader = (text, colIndex) => {
-    const newHeaders = [...columnHeaders];
-    newHeaders[colIndex] = text;
-    setColumnHeaders(newHeaders);
-  };
-
-  // Función para actualizar el valor de un encabezado de fila
-  const updateRowHeader = (text, rowIndex) => {
-    const newHeaders = [...rowHeaders];
-    newHeaders[rowIndex] = text;
-    setRowHeaders(newHeaders);
-  };
-
-  // Función para seleccionar una columna al mantener el clic
+  // Función para seleccionar una columna completa con long press
   const handleColumnLongPress = (colIndex) => {
     setSelectedColumn(colIndex);
     setSelectedRow(null);
-    setSelectedCell({ row: null, col: null }); // Limpiar selección de fila y celda
+    setSelectedCell({ row: null, col: null });
   };
 
-  // Función para seleccionar una fila al mantener el clic
+  // Función para seleccionar una fila completa con long press
   const handleRowLongPress = (rowIndex) => {
     setSelectedRow(rowIndex);
     setSelectedColumn(null);
-    setSelectedCell({ row: null, col: null }); // Limpiar selección de columna y celda
+    setSelectedCell({ row: null, col: null });
   };
 
-  // Función para seleccionar una celda específica
-  const handleCellPress = (rowIndex, colIndex) => {
+  // Función para seleccionar una celda específica con long press
+  const handleCellLongPress = (rowIndex, colIndex) => {
     setSelectedCell({ row: rowIndex, col: colIndex });
+    setSelectedRow(null);
     setSelectedColumn(null);
-    setSelectedRow(null); // Limpiar selección de fila y columna
   };
 
-  // Función para deseleccionar al hacer clic en cualquier lugar fuera de las celdas
+  // Limpiar selección cuando se toca fuera de cualquier celda o dentro de otra celda
   const clearSelection = () => {
     setSelectedColumn(null);
     setSelectedRow(null);
-    setSelectedCell({ row: null, col: null }); // Limpiar selección de celda
+    setSelectedCell({ row: null, col: null });
   };
 
   return (
     <TouchableWithoutFeedback onPress={clearSelection}>
-      <ScrollView
-        style={{ flex: 1, backgroundColor: "white", paddingVertical: 20 }}
-      >
-        {/* Botones para agregar filas y columnas */}
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: 20,
-          }}
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          style={{ flex: 1, backgroundColor: "white", paddingVertical: 20 }}
         >
-          <Button title="Agregar Fila" onPress={addRow} />
-          <Button title="Agregar Columna" onPress={addColumn} />
-        </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              gap: 4,
+              marginBottom: 20,
+              paddingHorizontal: 8,
+            }}
+          >
+            <TouchableOpacity onPress={addRow} style={styles.iconButton}>
+              <PlusCircle size={24} color={"darkgray"} />
+              <Text style={styles.iconText}>Fila</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={addColumn} style={styles.iconButton}>
+              <PlusCircle size={24} color={"darkgray"} />
+              <Text style={styles.iconText}>Columna</Text>
+            </TouchableOpacity>
+          </View>
 
-        {/* ScrollView horizontal para la tabla */}
-        <ScrollView horizontal>
-          <View>
-            {/* Encabezados de las columnas */}
-            <View style={{ flexDirection: "row" }}>
-              {/* Primera celda vacía en la esquina */}
-              <View
-                style={{
-                  width: 100,
-                  height: 50,
-                  backgroundColor: "#f0f0f0",
-                  borderWidth: 1,
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Text></Text>
-              </View>
-              {/* Celdas identificadoras de columnas (A, B, ...) */}
-              {columnHeaders.map((header, colIndex) => (
-                <TouchableOpacity
-                  key={colIndex}
-                  onPress={() => setEditingColumnIndex(colIndex)}
-                  onLongPress={() => handleColumnLongPress(colIndex)} // Detectar "long press" en la columna
+          {/* ScrollView horizontal para la tabla */}
+          <ScrollView horizontal>
+            <View>
+              {/* Encabezados de las columnas */}
+              <View style={{ flexDirection: "row" }}>
+                {/* Primera celda vacía en la esquina */}
+                <View
+                  style={{
+                    width: 100,
+                    height: 50,
+                    backgroundColor: "#f0f0f0",
+                    borderWidth: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
                 >
-                  <View
-                    style={[
-                      styles.cell,
-                      selectedColumn === colIndex && styles.selectedCell, // Estilo si la columna está seleccionada
-                      selectedColumn === colIndex && styles.selectedCellBorder, // Cambiar color del borde
-                    ]}
-                  >
-                    {editingColumnIndex === colIndex ? (
-                      <TextInput
-                        value={header}
-                        onChangeText={(text) =>
-                          updateColumnHeader(text, colIndex)
-                        }
-                        style={{
-                          padding: 5,
-                          width: 100,
-                          textAlign: "center",
-                        }}
-                        onBlur={() => setEditingColumnIndex(null)}
-                      />
-                    ) : (
-                      <Text>{header}</Text>
-                    )}
-                  </View>
-                </TouchableOpacity>
-              ))}
-            </View>
-
-            {/* Renderizar las filas con identificadores de fila */}
-            {rows.map((row, rowIndex) => (
-              <View key={rowIndex} style={{ flexDirection: "row" }}>
-                {/* Celda identificadora de fila (1, 2, 3, ...) */}
-                <TouchableOpacity
-                  onPress={() => setEditingRowIndex(rowIndex)}
-                  onLongPress={() => handleRowLongPress(rowIndex)} // Detectar "long press" en la fila
-                >
-                  <View
-                    style={[
-                      styles.cell,
-                      selectedRow === rowIndex && styles.selectedCell, // Estilo si la fila está seleccionada
-                      selectedRow === rowIndex && styles.selectedCellBorder, // Cambiar color del borde
-                    ]}
-                  >
-                    {editingRowIndex === rowIndex ? (
-                      <TextInput
-                        value={rowHeaders[rowIndex].toString()}
-                        onChangeText={(text) => updateRowHeader(text, rowIndex)}
-                        style={{
-                          padding: 5,
-                          width: 100,
-                          textAlign: "center",
-                        }}
-                        onBlur={() => setEditingRowIndex(null)}
-                      />
-                    ) : (
-                      <Text>{rowHeaders[rowIndex]}</Text>
-                    )}
-                  </View>
-                </TouchableOpacity>
-
-                {/* Celdas de la tabla */}
-                {row.map((cell, colIndex) => (
+                  <Text></Text>
+                </View>
+                {/* Celdas identificadoras de columnas (A, B, ...) */}
+                {columnHeaders.map((header, colIndex) => (
                   <TouchableOpacity
                     key={colIndex}
-                    onPress={() => handleCellPress(rowIndex, colIndex)} // Seleccionar una celda específica
+                    onPress={clearSelection} // Deseleccionar al tocar otra columna
+                    onLongPress={() => handleColumnLongPress(colIndex)} // Seleccionar toda la columna con long press
                   >
-                    <TextInput
-                      value={cell}
-                      onChangeText={(text) =>
-                        updateCell(text, rowIndex, colIndex)
-                      }
+                    <View
                       style={[
                         styles.cell,
-                        // Si la fila, columna o celda está seleccionada, cambiar el estilo de la celda
-                        selectedRow === rowIndex ||
-                        selectedColumn === colIndex ||
-                        (selectedCell.row === rowIndex &&
-                          selectedCell.col === colIndex)
-                          ? styles.selectedCell
-                          : null,
-                        // Cambiar color del borde también si la fila, columna o celda está seleccionada
-                        selectedRow === rowIndex ||
-                        selectedColumn === colIndex ||
-                        (selectedCell.row === rowIndex &&
-                          selectedCell.col === colIndex)
-                          ? styles.selectedCellBorder
-                          : null,
-                        // Añadir paddingLeft para el texto
-                        { paddingLeft: 10 },
+                        selectedColumn === colIndex && styles.selectedColumn,
                       ]}
-                    />
+                    >
+                      <Text>{header}</Text>
+                    </View>
                   </TouchableOpacity>
                 ))}
               </View>
-            ))}
-          </View>
+
+              {/* Renderizar las filas con identificadores de fila */}
+              {rows.map((row, rowIndex) => (
+                <View key={rowIndex} style={{ flexDirection: "row" }}>
+                  {/* Celda identificadora de fila (1, 2, 3, ...) */}
+                  <TouchableOpacity
+                    onPress={clearSelection} // Deseleccionar al tocar otra fila
+                    onLongPress={() => handleRowLongPress(rowIndex)} // Seleccionar toda la fila con long press
+                  >
+                    <View
+                      style={[
+                        styles.cell,
+                        selectedRow === rowIndex && styles.selectedRow,
+                      ]}
+                    >
+                      <Text>{rowHeaders[rowIndex]}</Text>
+                    </View>
+                  </TouchableOpacity>
+
+                  {/* Celdas de la tabla */}
+                  {row.map((cell, colIndex) => {
+                    const inputIndex = `${rowIndex}-${colIndex}`; // Crear un índice único para la celda
+                    return (
+                      <TouchableOpacity
+                        key={colIndex}
+                        onPress={() => {
+                          clearSelection(); // Limpiar selección
+                          inputRefs.current[inputIndex]?.focus(); // Activar el input al tocar la celda
+                        }}
+                        onLongPress={() =>
+                          handleCellLongPress(rowIndex, colIndex)
+                        } // Seleccionar una celda específica con long press
+                      >
+                        <View
+                          style={[
+                            styles.cell,
+                            selectedCell.row === rowIndex &&
+                            selectedCell.col === colIndex
+                              ? styles.selectedCell
+                              : null,
+                            // Aplicar el color azul para la fila o columna seleccionada
+                            selectedRow === rowIndex && styles.selectedRow,
+                            selectedColumn === colIndex &&
+                              styles.selectedColumn,
+                          ]}
+                        >
+                          <TextInput
+                            ref={(el) => (inputRefs.current[inputIndex] = el)} // Guardar la referencia de cada input
+                            value={cell}
+                            onPressIn={() => {
+                              clearSelection(); // Limpiar selección al tocar el input
+                              inputRefs.current[inputIndex]?.focus(); // Foco en el input
+                            }}
+                            onChangeText={(text) =>
+                              updateCell(text, rowIndex, colIndex)
+                            }
+                            style={{ padding: 5, textAlign: "center" }}
+                          />
+                        </View>
+                      </TouchableOpacity>
+                    );
+                  })}
+                </View>
+              ))}
+            </View>
+          </ScrollView>
         </ScrollView>
-      </ScrollView>
+      </View>
     </TouchableWithoutFeedback>
   );
 }
 
-// Estilos para las celdas seleccionadas
+// Estilos para las celdas seleccionadas y botones de icono
 const styles = StyleSheet.create({
   cell: {
     width: 100,
     height: 50,
     backgroundColor: "#f0f0f0",
     borderWidth: 1,
-    borderColor: "#000", // Color del borde por defecto
+    borderColor: "#000",
     justifyContent: "center",
     alignItems: "center",
   },
-  selectedCell: {
-    backgroundColor: "#d0eaff", // Color de fondo cuando está seleccionada
+  selectedRow: {
+    backgroundColor: "#d0eaff", // Color para toda la fila seleccionada (azul)
   },
-  selectedCellBorder: {
-    borderColor: "#4A90E2", // Cambiar color del borde cuando está seleccionada
+  selectedColumn: {
+    backgroundColor: "#d0eaff", // Color para toda la columna seleccionada (azul)
+  },
+  selectedCell: {
+    backgroundColor: "#a4de02", // Color verde para celdas seleccionadas individualmente
+  },
+  iconButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
+  },
+  iconText: {
+    marginLeft: 5,
   },
 });
